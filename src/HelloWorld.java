b5151c0531d5e8f5003ecab96c1a0cd56aa713ef
Gustavo <gustavo.ribeiro0988@gmail.com>
2026-09-21 18:14:43 -0300

    alterações projeto

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,41 +3,18 @@ import java.util.Scanner;
 public class HelloWorld {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
 
-System.out.println("Olá, seja bem vindo!");
-Scanner scanner= new Scanner(System.in);
-
-        System.out.println("Qual seu nome completo?");
-        String nome= scanner.nextLine();
-
-        System.out.println("Qual seu CPF?");
-        String CPF= scanner.nextLine();
-
-        System.out.println("Qual sua idade?");
-        int idade= scanner.nextInt();
-
-        System.out.println("Qual seu peso?");
-        double peso= scanner.nextDouble();
-
-        System.out.println("Qual sua altura?");
-        double altura= scanner.nextDouble();
-
-        System.out.println("Qual seu estado civil?");
-        String estadocivil= scanner.nextLine();
-
-        scanner.close();
-
-        System.out.println(" O seu nome é " + nome + " o seu CPF é " + CPF+ " A sua idade é " + idade + " O seu peso é " + peso +
-                " A sua altura é " + altura + " Seu estado civil é " + estadocivil);
-
-
-
-
-
-
-
-
-
-
+        if (num1 > num2) {
+            System.out.println("O primeiro numero é maior");
+        }
+        else if (num2 > num1) {
+            System.out.println("O segundo número é maior");
+        }
+        else {
+            System.out.println("Os numeros sao iguais");
+        }
     }
 }
